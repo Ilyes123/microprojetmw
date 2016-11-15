aea263b6fb97fde18f04bbb05a16a2b09134d50f
Dhia Eddine CHOUCHANE <chouchan@b313-11.int-evry.fr>
2016-11-15 20:49:08 +0100

    These probably shouldn't be there

--- Reservation/entity/NewsGroupRight_.java
+++ Reservation/entity/NewsGroupRight_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150916-rNA", date="2016-11-15T20:44:57")
+@Generated(value="EclipseLink-2.6.1.v20150916-rNA", date="2016-11-15T20:48:29")
 @StaticMetamodel(NewsGroupRight.class)
 public class NewsGroupRight_ { 
 
